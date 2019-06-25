dbfc0cb6d6394a5177dc7e32803748218d088233
kurogane153 <11807067@stu.kbc.ac.jp>
2019-06-25 14:34:41 +0900

    ジャンプ処理変更後

--- mario.cpp
+++ mario.cpp
@@ -1,9 +1,14 @@
 #include "DxLib.h"
-#define HEIGHT 20
+#include "math.h"
+#include <string>
+#define HEIGHT 200
 #define WIDTH 15
 #define SIZE 32
 
-const float G=9.8;
+const float G=9.8067;
+const float y_max=2.000;
+const float v0=4.0624;
+const float v=-0.126953125;
 
 /*列挙体*/
 typedef enum GAME_MODE{
@@ -21,6 +26,7 @@ typedef	enum PlayerDire{
 	DOWN,
 	LEFT,
 	RIGHT,
+	JUMP,
 	NL,
 };
 //構造体
@@ -32,37 +38,39 @@ struct Field{
 };
 struct Player{
 	int x;
-	int y;
+	float y;
 	int dire;
+	int JumpFlg;
 	int img[15];
 };
 
 struct Player	g_player;
-struct Field	g_Field[HEIGHT][WIDTH];
+struct Field	g_Field[WIDTH][HEIGHT];
+char FildDete[HEIGHT][WIDTH];
 int FieldDate[HEIGHT][WIDTH]=
 {
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//0.0
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//1.0
-	{0,0,0,0,0,0,0,0,18,30,0,0,0,5,6},//2.0
-	{0,0,0,0,0,0,0,0,19,31,0,0,0,5,6},//3.0
-	{0,0,0,0,0,0,0,0,19,31,0,0,0,5,6},//4.0
-	{0,0,0,0,0,0,0,0,20,32,0,0,0,5,6},//5.0
-	{0,0,0,0,0,0,0,0,0,0,0,0,14,5,6},//6.0
-	{0,0,0,0,0,0,0,0,0,0,0,0,15,5,6},//7.0
-	{0,0,0,0,0,0,0,0,0,0,0,0,16,5,6},//8.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//2.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//3.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//4.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//5.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//6.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//7.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//8.0
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//9.0
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//10.0
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//11.0
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//12.0
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//13.0
-	{0,0,0,0,0,0,0,0,0,0,0,0,14,5,6},//14.0
-	{0,0,0,0,0,0,0,0,0,0,0,0,15,5,6},//15
-	{0,0,0,0,0,0,0,0,0,0,0,0,16,5,6},//16
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//17
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//18
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//19
-};
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//14.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//13.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//14.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//13.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//14.0
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,5,6},//13.0
 
+};
 
 //グローバル変数
 int g_GameState=GAME_INT;
@@ -81,6 +89,7 @@ void DrawField(void);
 void DrawPlayer(void);
 void PlayerControl(void);
 int CollisionDetection(int i);
+int ReadSteag(void);
 
 //メイン
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
@@ -133,10 +142,40 @@ int LoadImages(void)
 	return 0;
 }
 
+/*ランキングデータ読み込み*/
+int ReadSteag(void)
+{
+	FILE *fp;
+	#pragma warning(disable:4996)
+
+	//ファイルオープン
+	if((fp = fopen("mario.txt","r"))== NULL){
+		//エラー処理
+		printf("Ranking Data Error\n");
+		return -1;
+	}
+
+	//ランキングデータ配分列データを読み込む
+	for(int i=0;i<HEIGHT;i++){
+		for(int j=0;i<WIDTH;i++){
+			fscanf(fp, "%d",FieldDate[j][i]);
+			//FieldDate[j][i]=FildDete[i][j];
+			g_Field[j][i].x=j;
+			g_Field[j][i].y=i;
+		}
+	}
+	
+	//ファイルクローズ
+	fclose(fp);
+
+	return 0;
+}
+
 //初期処理
 void GameInit(void)
 {
-	FieldInit();
+	//ReadSteag();
+	//FieldInit();
 	PlayerInit();
 	g_GameState=GAME_MAIN;
 }
@@ -145,7 +184,8 @@ void GameInit(void)
 void PlayerInit(void)
 {
 	g_player.x=64;
-	g_player.y=384;
+	g_player.y=384.0;
+	g_player.JumpFlg=0;
 }
 
 
@@ -154,9 +194,9 @@ void FieldInit(void)
 {
 	for(int i=0;i<HEIGHT;i++){
 		for(int j=0;j<WIDTH;j++){
-			g_Field[i][j].x=j;
-			g_Field[i][j].y=i;
-			g_Field[i][j].flg=FieldDate[i][j];
+			g_Field[j][i].x=j;
+			g_Field[j][i].y=i;
+			g_Field[j][i].flg=FieldDate[j][i];
 		}
 	}
 }
@@ -174,88 +214,131 @@ void DrawField(void)
 {
 	for(int i=0; i < HEIGHT; i++){
 		for(int j=0; j < WIDTH; j++){
-			if(g_Field[i][j].flg==0){
+			if(g_Field[j][i].flg==0){
 				DrawGraph(i*SIZE,j*SIZE,g_FiledImage[84],TRUE);
-			}else if(g_Field[i][j].flg==1){
-				DrawGraph(i*SIZE,j*SIZE,g_FiledImage[5],TRUE);
 			}else{
 				DrawGraph(i*SIZE,j*SIZE,g_FiledImage[FieldDate[i][j]],TRUE);
 			}
 		}
 	}
+	for(int i=0; i < HEIGHT; i++){
+		for(int j=0; j < WIDTH; j++){
+			DrawFormatString(i*SIZE+10,j*SIZE+10, 0xff00000,"%d",FieldDate[i][j]);
+		}
+	}
+	DrawFormatString(500,80, 0xff00000,"%d",g_player.x);
+	DrawFormatString(500,100, 0xff00000,"%d",g_player.y);
 }
 
 //プレイヤー描画
 void DrawPlayer(void)
 {
-	DrawGraph(g_player.x,g_player.y,g_player.img[0],TRUE);
+	static int cnt=0;
+	if(g_NowKey){
+		if(g_player.dire==RIGHT){
+			if(cnt<=4){
+				DrawGraph(g_player.x,g_player.y,g_player.img[1],TRUE);
+			}else if(cnt<=8){
+				DrawGraph(g_player.x,g_player.y,g_player.img[2],TRUE);
+			}else if(cnt>8){
+				DrawGraph(g_player.x,g_player.y,g_player.img[1],TRUE);
+				cnt=0;
+			}
+		}else if(g_player.dire==LEFT){
+			if(cnt<=4){
+				DrawTurnGraph(g_player.x,g_player.y,g_player.img[1],TRUE);
+			}else if(cnt<=8){
+				DrawTurnGraph(g_player.x,g_player.y,g_player.img[2],TRUE);
+			}else if(cnt>8){
+				DrawTurnGraph(g_player.x,g_player.y,g_player.img[1],TRUE);
+				cnt=0;
+			}
+		}else if(g_player.dire==JUMP){
+			if(cnt<=4){
+				DrawGraph(g_player.x,g_player.y,g_player.img[3],TRUE);
+			}else if(cnt<=8){
+				DrawGraph(g_player.x,g_player.y,g_player.img[4],TRUE);
+			}else if(cnt>8){
+				DrawGraph(g_player.x,g_player.y,g_player.img[4],TRUE);
+				cnt=5;
+			}
+		}
+	}else{
+		DrawGraph(g_player.x,g_player.y,g_player.img[0],TRUE);
+	}
+	cnt++;
 }
 
 //プレイヤー移動
 void PlayerControl(void)
 {
-	static int x,y,cnt=0;
-	int k[4];
+	static int y_prev,y_temp,faib,cnt=0;
 	if(g_KeyFlg & PAD_INPUT_UP)	g_player.dire=UP;
 	if(g_KeyFlg & PAD_INPUT_RIGHT)	g_player.dire=RIGHT;
 	if(g_KeyFlg & PAD_INPUT_DOWN)	g_player.dire=DOWN;
 	if(g_KeyFlg & PAD_INPUT_LEFT)	g_player.dire=LEFT;
-	if(g_NowKey){
-		if(g_player.dire==LEFT)	g_player.x-=2;
-		if(g_player.dire==RIGHT)	g_player.x+=2;
-	}
-	for(int i=0;i<4;i++){
-		k[i]=CollisionDetection(i);
-	}
-	for(int i=0;i<4;i++){
-		if(k[i]==1){
-
+	if(g_KeyFlg & PAD_INPUT_M){
+		g_player.dire=JUMP;
+		if(g_player.JumpFlg==0){
+			g_player.JumpFlg=1;
+			y_prev=g_player.y;
+			g_player.y=g_player.y-16;
 		}
-
 	}
-}
-
-//当たり判定
-int CollisionDetection(int i)
-{
-	static int x,y;
-	switch (i)
+	if(!g_NowKey)	g_player.dire=NL;
+	switch (g_player.dire)
 	{
-	case 0:
-		x=g_player.x/SIZE;
-		y=g_player.y/SIZE;
-		if(g_Field[y][x].flg==0){
-			return 0;
-		}else{
-			return 1;
+	case RIGHT:
+		if(g_Field[(int)g_player.y/SIZE][g_player.x/32+1].flg==0){ //右の当たり判定
+			int posX=g_player.x+32;
+			int posY=g_player.y;
+			DrawBox(posX,posY,posX+32,posY+32,GetColor(255,0,0),FALSE);
+			g_player.x+=4;
 		}
 		break;
-	case 1:
-		x=g_player.x+SIZE/SIZE;
-		y=g_player.y/SIZE;
-		if(g_Field[y][x].flg==0){
-			return 0;
-		}else{
-			return 1;
+	case LEFT:
+		if(g_Field[(int)g_player.y/32][g_player.x/32-1].flg==0){ //左の当たり判定
+			int posX=g_player.x-32;
+			int posY=g_player.y;
+			DrawBox(posX,posY,posX+32,posY+32,GetColor(255,0,0), FALSE );
+			g_player.x-=4;
 		}
 		break;
-	case 2:
-		x=g_player.x/SIZE;
-		y=g_player.y+SIZE/SIZE;
-		if(g_Field[y][x].flg==0){
-			return 0;
-		}else{
-			return 1;
+	case UP:
+		if(g_Field[(int)g_player.y/32-1][g_player.x/32].flg==0){ //上の当たり判定
+			int posX=g_player.x;
+			int posY=g_player.y-32;
+			DrawBox(posX,posY,posX+32,posY+32,GetColor(255,0,0), FALSE );
 		}
 		break;
-	case 3:
-		x=g_player.x+SIZE/SIZE;
-		y=g_player.y+SIZE/SIZE;
-		if(g_Field[y][x].flg==0){
-			return 0;
-		}else{
-			return 1;
+	case DOWN:
+		if(g_Field[(int)g_player.y/32+1][g_player.x/32].flg==0||g_Field[(int)g_player.y/32+1][g_player.x/32].flg==0){ //下の当たり判定
+			int posX=g_player.x;
+			int posY=g_player.y+32;
+			DrawBox(posX,posY,posX+32,posY+32,GetColor(255,0,0), FALSE );
+		}
+		break;
+	case JUMP:
+		if(g_Field[(int)g_player.y/32-1][g_player.x/32].flg==0){
+			int posX=g_player.x;
+			int posY=g_player.y-32;
+			DrawBox(posX,posY,posX+32,posY+32,GetColor(255,0,0), FALSE );
 		}
 		break;
 	}
+	if(g_Field[(int)g_player.y/32+1][g_player.x/32].flg==0&&g_player.JumpFlg!=1){ //下の当たり判定
+		g_player.y+=4;
+	}
+	if(g_player.JumpFlg==1){
+		if(g_Field[(int)g_player.y/32-1][g_player.x/32].flg==0){
+			y_temp=g_player.y;
+			g_player.y+=(g_player.y-y_prev)+1;
+			y_prev=y_temp;
+			if(g_Field[(int)g_player.y/32+1][g_player.x/32].flg==5)g_player.JumpFlg=0;
+			if(g_player.y==384){
+				g_player.JumpFlg=0;
+			}
+		}
+	}
+	DrawFormatString(500,120, 0xff00000,"%d",g_player.JumpFlg);
 }
